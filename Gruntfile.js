087a4c997584e1c4876e1d8926a51ba03c328bf1
junghyun <arunson@kaist.ac.kr>
2018-01-25 10:30:50 +0900

    Use different way to make failing test more visible in Sauce

--- Gruntfile.js
+++ Gruntfile.js
@@ -393,7 +393,7 @@ module.exports = function (grunt) {
           build: process.env.TRAVIS_JOB_ID,
           concurrency: 10,
           maxRetries: 3,
-          urls: ['http://127.0.0.1:3000/js/tests/index.html'],
+          urls: ['http://127.0.0.1:3000/js/tests/index.html??hidepassed'],
           browsers: grunt.file.readYAML('grunt/sauce_browsers.yml')
         }
       }
